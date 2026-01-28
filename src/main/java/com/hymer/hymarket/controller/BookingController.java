75fe11f2850813021ad697656ccb81d451498bfb
Singhashish-commits <singhashish.cs@gmail.com>
2026-01-29 00:09:56 +0530

    Fix typo in controller API endpoint

--- src/main/java/com/hymer/hymarket/controller/BookingController.java
+++ src/main/java/com/hymer/hymarket/controller/BookingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/bookingsb")
+@RequestMapping("/api/bookings")
 public class BookingController {
     private  final BookingService bookingService;
     @Autowired
