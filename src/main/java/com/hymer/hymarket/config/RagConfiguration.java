22510b9c6ee268146a50d93de8bfb57a56b500cb
Singhashish-commits <singhashish.cs@gmail.com>
2026-04-05 00:04:07 +0530

    Chat Bot  intergrtion not complete yet

---
+++ src/main/java/com/hymer/hymarket/config/RagConfiguration.java
@@ -0,0 +1,28 @@
+package com.hymer.hymarket.config;
+
+
+import org.springframework.ai.embedding.EmbeddingModel;
+//import org.springframework.ai.vectorstore.SimpleVectorStore;
+//import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class RagConfiguration {
+    @Value("classpath:Docs/PlatformRules.txt")
+    private Resource rulesDocument;
+//
+//    @Bean
+//    public VectorStore vectorStore(EmbeddingModel embeddingModel){
+//        SimpleVectorStore vectorStore = new SimpleVectorStore(embeddingModel);
+//            return vectorStore;
+//
+//    }
+
+
+
+}
+
+
