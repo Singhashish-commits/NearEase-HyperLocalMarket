ac2982abf479f9314f45d973a4258e7cedfa4e25
Singhashish-commits <singhashish.cs@gmail.com>
2026-05-31 18:45:49 +0530

    Fixed the DashBoard Issue

--- src/main/java/com/hymer/hymarket/Repository/ProviderProfileRepository.java
+++ src/main/java/com/hymer/hymarket/Repository/ProviderProfileRepository.java
@@ -3,6 +3,7 @@ package com.hymer.hymarket.Repository;
 import com.hymer.hymarket.model.ProviderProfile;
 import com.hymer.hymarket.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,10 +12,14 @@ import java.util.Optional;
 
 @Repository
 public interface ProviderProfileRepository  extends JpaRepository<ProviderProfile,Long> {
+
     Optional<ProviderProfile> findByUser(User user);
 
-    Double getRatingById(long id);
-    Double findAverageRatingById(Long id);
+    @Query("SELECT p.averageRating FROM ProviderProfile p WHERE p.id = :id")
+    Double getRatingById(@Param("id") long id);
+
+    @Query("SELECT p.averageRating FROM ProviderProfile p WHERE p.id = :id")
+    Double findAverageRatingById(@Param("id") Long id);
 
     List<ProviderProfile> findByIsVerified(boolean isVerified);
 }
