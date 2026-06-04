361588b486e84c21308e5edcc812ebdd8214ae01
Singhashish-commits <singhashish.cs@gmail.com>
2026-06-04 08:44:49 +0530

    Updated ofr the secured Endpoint

--- src/main/java/com/hymer/hymarket/config/RateLimitInterceptor.java
+++ src/main/java/com/hymer/hymarket/config/RateLimitInterceptor.java
@@ -9,6 +9,9 @@ import org.redisson.api.RedissonClient;
 import org.redisson.client.RedisClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -22,8 +25,19 @@ public class RateLimitInterceptor  implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-                String clientIp = getClentIp(request);
-                String limiterKey= "rate_limiter:" + clientIp;
+
+        String clientId;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()
+                && !(authentication instanceof AnonymousAuthenticationToken)) {
+            String email =  authentication.getName();
+            clientId = "user:" + email;
+
+        }
+        else {
+            clientId = "ip:" + getClientIP(request);
+        }
+                String limiterKey= "rate_limiter:" + clientId;
                 RRateLimiter rateLimiter = redissonClient.getRateLimiter(limiterKey);
                 rateLimiter.trySetRate(RateType.OVERALL, 5, 1, RateIntervalUnit.SECONDS);
                 boolean isAllowed = rateLimiter.tryAcquire(1);
@@ -41,7 +55,7 @@ public class RateLimitInterceptor  implements HandlerInterceptor {
 
 
 
-    private String getClentIp(HttpServletRequest request) {
+    private String getClientIP(HttpServletRequest request) {
         String xfHeader = request.getHeader("x-forwarded-for");
         if (xfHeader == null || xfHeader.isEmpty() || !xfHeader.contains(request.getRemoteAddr())) {
             xfHeader = request.getRemoteAddr();
