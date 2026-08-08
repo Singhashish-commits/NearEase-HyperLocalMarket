a72864f3b623358bb62322e5a69afcd4921a2206
Singhashish-commits <singhashish.cs@gmail.com>
2026-08-08 19:29:42 +0530

    ElasticSearch 3.0

--- src/main/java/com/hymer/hymarket/config/ElasticConfig.java
+++ src/main/java/com/hymer/hymarket/config/ElasticConfig.java
@@ -10,16 +10,11 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 import org.apache.http.HttpResponseInterceptor;
-import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class ElasticConfig {
